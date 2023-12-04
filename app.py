fe31bed1cf47d6bb815741ebd8603b6ef28b3169
miloszpaul <milosz.rosinski@gmail.com>
2023-12-04 12:40:23 +0100

    added camera function; requirements & packages

--- app.py
+++ app.py
@@ -1,43 +1,53 @@
-# Import necessary libraries
 import streamlit as st
-import numpy as np
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import cv2
 from PIL import Image
+import numpy as np
+import tempfile
+import zoom
 
-# Import functions from zoom.py
-from zoom import process_image, yolo, face_complete_distance_center_light, glass_detection
-
-# If using a ML model, import TensorFlow, PyTorch, etc.
-# from tensorflow.keras.models import load_model
-
-# Set up the logo, title and introduction of the app
-st.image("images/logo.png", width=150,)
+# Set up the logo, title, and introduction of the app
+st.image("images/logo.png", width=150)
 st.title('First Impression for Videocalls')
-st.write('This application provides insights into your first impression in videocalls based on the uploaded image.')
+st.write('This application provides insights into your first impression in videocalls based on the uploaded image or live camera feed.')
 
-# Image Upload Section
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-if uploaded_file is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Your Uploaded Image', use_column_width=True)
+# Image Upload or Camera Feed Section
+option = st.radio("Choose input method:", ("Upload Image", "Use Camera"))
 
-    # Convert the uploaded file to a format that can be processed by zoom.py
-    # For example, save the image to a temporary file
-    with open("temp_image.jpg", "wb") as f:
-        f.write(uploaded_file.getbuffer())
+if option == "Upload Image":
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Your Uploaded Image', use_column_width=True)
 
-    # Call the process_image function from zoom.py
-    process_image("temp_image.jpg")
+        # Save the uploaded image to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
+            image.save(tmp_file)
+            tmp_file_name = tmp_file.name
 
-    # Display results - Modify this part based on how you want to present the results
-    st.subheader('Analysis Results')
-    st.write('Based on our analysis...')
-    # You might want to modify process_image in zoom.py to return results instead of printing them
-else:
-    st.warning('Please upload an image.')
+        # Process the image using zoom.py
+        results = zoom.process_image(tmp_file_name)
+        st.write(results)
+
+elif option == "Use Camera":
+    class VideoProcessor(VideoTransformerBase):
+        def recv(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+
+            # Save the frame to a temporary file
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
+                cv2.imwrite(tmp_file.name, img)
+                tmp_file_name = tmp_file.name
+
+            # Process the image using zoom.py
+            results = zoom.process_image(tmp_file_name)
+            st.write(results)
+            return frame
+
+    webrtc_ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
 
 # Additional Guidelines (if needed)
 st.subheader('Guidelines')
 st.image("images/background.png", width=350)
 st.write('Here are some guidelines for selecting appropriate images for analysis...')
-# Add more instructions or use st.image, st.video for additional resources.
